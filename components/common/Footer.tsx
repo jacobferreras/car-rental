3a5ef1ec8db822bf74b43e03db50af2e1e5fc262
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-06-14 01:27:37 +0800

    feat: add Navbar and Footer components with initial layout

---
+++ components/common/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 left-0 footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10">
+      <aside>
+        <p>
+          Copyright © {new Date().getFullYear()} - All right reserved by ACME
+          Industries Ltd
+        </p>
+      </aside>
+    </footer>
+  );
+};
+
+export default Footer;
