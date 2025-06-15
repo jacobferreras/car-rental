accdad6b4b1d468a0128db4d9271cc5337959973
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-06-15 13:42:18 +0800

    fix: update Footer component styling for improved visibility

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 left-0 footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10">
+    <footer className="bottom-0 left-0 footer footer-horizontal footer-center bg-base-300 text-base-content rounded p-10">
       <aside>
         <p>
           Copyright © {new Date().getFullYear()} - All right reserved by ACME
