f5fbf7a2d791e0f668afebaf29ce0991e9d73b66
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-06-17 07:05:31 +0800

    refactor: simplify Dropdown component by removing unused props and hardcoding options

--- components/common/Dropdown.tsx
+++ components/common/Dropdown.tsx
@@ -3,16 +3,6 @@
 interface DropdownProps {
   value: string;
   onChange?: (event: React.ChangeEvent<HTMLSelectElement>) => void;
-  options: string;
-  options1: string;
-  options2: string;
-  options3?: string;
-  options4?: string;
-  placeholder1?: string;
-  placeholder2?: string;
-  placeholder3?: string;
-  placeholder4?: string;
-  desriptions?: string[];
 }
 
 const Dropdown = (props: DropdownProps) => {
@@ -23,12 +13,12 @@ const Dropdown = (props: DropdownProps) => {
       className="select rounded-xl font-normal mr-5 w-50 bg-neutral-700 text-white"
     >
       <option value="" className="text-white">
-        {props.options}
+        All Seats
       </option>
-      <option value={props.placeholder1}>{props.options1}</option>
-      <option value={props.placeholder2}>{props.options2}</option>
-      <option value={props.placeholder3}>{props.options3}</option>
-      <option value={props.placeholder4}>{props.options4}</option>
+      <option value="2">2 Seater</option>
+      <option value="4">4 Seater</option>
+      <option value="5">5 Seater</option>
+      <option value="7">7 Seater</option>
     </select>
   );
 };
