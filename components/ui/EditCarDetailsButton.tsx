b6003a9a13b1b992bb25fc1dbf2e11b14d7e42d7
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-06-18 22:38:31 +0800

    feat: add EditCarDetailsButton component for updating car details

---
+++ components/ui/EditCarDetailsButton.tsx
@@ -0,0 +1,29 @@
+import { useState, useEffect, use } from "react";
+
+interface EditCarDetailsButtonProps {
+  onClick?: () => void;
+}
+
+const EditCarDetailsButton = (props: EditCarDetailsButtonProps) => {
+  const [isGranted, setIsGranted] = useState(false);
+
+  useEffect(() => {
+    fetch("/api/permission")
+      .then((res) => res.json())
+      .then((data) => setIsGranted(data.isGranted));
+  }, []);
+  return (
+    <div>
+      {isGranted ? (
+        <button
+          className="btn bg-neutral-700 rounded-xl text-white"
+          onClick={props.onClick}
+        >
+          Update Car Details
+        </button>
+      ) : null}
+    </div>
+  );
+};
+
+export default EditCarDetailsButton;
