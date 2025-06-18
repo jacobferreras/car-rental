dec973cd5060110868a85cd5e1cd3cc3b3d8b9d1
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-06-18 15:04:25 +0800

    feat: update image configuration to allow remote patterns for any domain

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "lh3.googleusercontent.com",
-      "www.gravatar.com",
-      "hips.hearstapps.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // allows any domain
+      },
     ],
   },
 };
