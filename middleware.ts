fc3919246252b7e57ee997176b55893b02581739
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-06-20 08:19:58 +0800

    fix: update middleware to include /api/permission in publicPaths

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,7 @@
 import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
 
 export default withAuth(async function middleware() {}, {
-  // Middleware still runs on all routes, but doesn't protect the blog route
-  publicPaths: ["/", "/api/cars"],
+  publicPaths: ["/", "/api/cars", "/api/permission"],
 });
 
 export const config = {
