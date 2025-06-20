e6520b012131fb62c95c1ceb23f3b063a23a40e8
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-06-20 14:09:30 +0800

    feat: implement custom hook for fetching car model details

---
+++ hooks/useCarModel.tsx
@@ -0,0 +1,42 @@
+import React, { use } from "react";
+import { fetchCarModel } from "@/services/fetchCarModel";
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+
+interface Car {
+  id: number;
+  make: string;
+  model: string;
+  year: number;
+  seats: number;
+  transmission: string;
+  imageUrl: string;
+  status: string;
+  pricePerDay: number;
+  fuelType: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+const useCarModel = () => {
+  const { model } = useParams();
+  const [car, setCar] = useState<Car | null>(null);
+
+  useEffect(() => {
+    const getCarModel = async () => {
+      try {
+        const data = await fetchCarModel(model as string);
+        setCar(data);
+      } catch (error) {
+        console.error("Failed to fetch car model:", error);
+      }
+    };
+
+    if (model) {
+      getCarModel();
+    }
+  }, [model]);
+  return { car, setCar };
+};
+
+export default useCarModel;
