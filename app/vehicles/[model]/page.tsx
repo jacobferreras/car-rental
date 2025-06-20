b7b3fe14380093cf27b10fdf6f6e18b92c45b510
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-06-20 16:46:45 +0800

    feat: update layout of car details page for improved structure and styling

--- app/vehicles/[model]/page.tsx
+++ app/vehicles/[model]/page.tsx
@@ -12,29 +12,39 @@ const page = () => {
   }
 
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <div className="card lg:card-side bg-base-100 shadow-sm">
-        <figure>
-          <Image
-            src={car.imageUrl.trim()}
-            alt={car.make}
-            className="h-60 w-full"
-            width="1000"
-            height="1000"
-          />
-        </figure>
-        <div className="card-body w-full">
-          <h2 className="card-title text-6xl">
-            <span>{car.make}</span>
-            {car.model}
-          </h2>
-          <div>
-            <p>STARTING AT</p>
-            <p className="text-2xl">₱{car.pricePerDay}/Day</p>
+    <>
+      <div className="min-h-screen flex flex-col justify-center items-center px-4 pt-32">
+        <div className="card lg:card-side bg-base-100 shadow-sm">
+          <figure>
+            <Image
+              src={car.imageUrl.trim()}
+              alt={car.make}
+              className="h-60 w-full"
+              width="1000"
+              height="1000"
+            />
+          </figure>
+          <div className="card-body w-full">
+            <h2 className="card-title text-6xl">
+              <span>{car.make}</span>
+              {car.model}
+            </h2>
+            <div>
+              <p>STARTING AT</p>
+              <p className="text-2xl">₱{car.pricePerDay}/Day</p>
+            </div>
+            <div>
+              <p className="text-xl">{car.description}</p>
+            </div>
+            <div>
+              <button className="btn btn-primary">Rent Now</button>
+            </div>
           </div>
         </div>
+
+        <div className="text-6xl py-6">Car Details</div>
       </div>
-    </div>
+    </>
   );
 };
 
