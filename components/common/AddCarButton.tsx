616c04517aeed9f5cc556f6386b7d08d4392162c
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-06-24 18:52:50 +0800

    feat: implement AddCarButton component with modal for adding cars

--- components/common/AddCarButton.tsx
+++ components/common/AddCarButton.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
+import useCards from "@/hooks/useCards";
+import AddCarModal from "./AddCarModal";
 
 interface AddCarButtonProps {
   onClick?: () => void;
@@ -7,6 +9,11 @@ interface AddCarButtonProps {
 
 export const AddCarButton = (props: AddCarButtonProps) => {
   const [isGranted, setIsGranted] = useState(false);
+  const [refresh, setRefresh] = useState(0);
+  const [openModal, setOpenModal] = useState(false);
+  const {} = useCards({
+    refresh,
+  });
 
   useEffect(() => {
     fetch("/api/permission")
@@ -18,11 +25,22 @@ export const AddCarButton = (props: AddCarButtonProps) => {
       {isGranted ? (
         <button
           className="btn bg-neutral-700 rounded-xl text-white"
-          onClick={props.onClick}
+          onClick={() => setOpenModal(true)}
         >
           Add Car
         </button>
       ) : null}
+
+      <div>
+        <AddCarModal
+          open={openModal}
+          onClose={() => setOpenModal(false)}
+          onSuccess={() => {
+            setRefresh((prev) => prev + 1);
+            setOpenModal(false);
+          }}
+        />
+      </div>
     </div>
   );
 };
