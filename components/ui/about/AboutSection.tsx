95881f53ebdfb3696530f3ef142a26b27d286b8c
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-06-27 19:08:33 +0800

    fix: clean up AboutSection component by removing unnecessary classes and updating text

--- components/ui/about/AboutSection.tsx
+++ components/ui/about/AboutSection.tsx
@@ -4,24 +4,23 @@ import Image from "next/image";
 const AboutSection = () => {
   return (
     <>
-      <div className="hero min-h-100 relative hero-overlay bg-overlay-opacity-60">
+      <div className="hero min-h-100 relative">
         <Image
           src="/About.png"
           alt="Background"
           fill
-          className="object-cover z-0 "
+          className="object-cover z-0"
           priority
         />
-        <div className="flex flex-col justify-center items-center absolute">
-          <h1 className="text-3xl md:text-5xl lg:text-7xl text-center text-white font-family-mono font-bold z-20">
-            DRIVE YOUR JOURNEY
+        {/* Dark overlay */}
+        <div className="absolute inset-0 bg-black opacity-60 z-0"></div>
+        <div className="flex flex-col justify-center items-center absolute z-20">
+          <h1 className="text-3xl md:text-5xl lg:text-7xl text-center text-white font-family-mono font-bold">
+            ABOUT INSTADRIVE
           </h1>
-          <h2 className="text-3xl md:text-5xl lg:text-6xl text-center text-white font-family-mono font-bold z-20">
+          <h2 className="text-3xl md:text-5xl lg:text-6xl text-center text-white font-family-mono font-bold">
             WITH INSTADRIVE
           </h2>
-          <div className="flex justify-center items-center mt-4">
-            <button className="btn bg-red-600 text-white z-20">Book Now</button>
-          </div>
         </div>
       </div>
     </>
