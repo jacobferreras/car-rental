7758dae80cfc039def33e5101ec9468baf1dc74b
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-06-29 15:21:53 +0800

    refactor: update MissionAndVisionCard layout for improved structure and responsiveness

--- components/ui/about/MissionAndVisionCard.tsx
+++ components/ui/about/MissionAndVisionCard.tsx
@@ -7,17 +7,18 @@ interface MissionAndVisionCardProps {
 const MissionAndVisionCard = (props: MissionAndVisionCardProps) => {
   return (
     <>
-      <div className="card bg-base-100 w-auto md:h-70 xl:h-90 lg:w-96 shadow-sm">
-        <figure className="px-10 pt-4">
+      <div className="card bg-base-100 w-auto md:h-90 xl:h-90 lg:w-110 shadow-sm">
+        <div className="card-header flex justify-center items-center pt-4 h-20 md:h-24 lg:h-32 xl:h-40">
           <i
             className={`${props.iconClass} text-xl text-red-600 md:text-3xl xl:text-5xl`}
           ></i>
-        </figure>
+        </div>
+
         <div className="card-body items-center text-center">
           <h1 className="font-family-mono text-md md:text-lg xl:text-xl font-bold">
             {props.title}
           </h1>
-          <p className="font-family-sans text-xs md:text-md xl:text-lg text-justify ">
+          <p className="font-family-sans text-xs md:text-lg xl:text-lg text-justify ">
             {props.description}
           </p>
         </div>
