3f44267d4cd954841c1c03ff21c0962777996bca
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-06-30 13:45:46 +0800

    feat: update OurteamCard component to use props for dynamic content

--- components/ui/about/OurteamCard.tsx
+++ components/ui/about/OurteamCard.tsx
@@ -1,26 +1,25 @@
 interface OurteamCardProps {
   imageUrl?: string;
-  title?: string;
+  name?: string;
+  position?: string;
   description?: string;
 }
 
 const OurteamCard = (props: OurteamCardProps) => {
   return (
     <>
-      <div className="card bg-base-100 w-96 shadow-sm">
+      <div className="card bg-base-100 w-auto shadow-sm">
         <figure className="px-10 pt-10">
           <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes"
-            className="rounded-xl"
+            src={props.imageUrl}
+            alt={props.name}
+            className="rounded-full w-32 h-32 object-cover"
           />
         </figure>
         <div className="card-body items-center text-center">
-          <h2 className="card-title">Card Title</h2>
-          <p>
-            A card component has a figure, a body part, and inside body there
-            are title and actions parts
-          </p>
+          <h2 className="card-title">{props.name}</h2>
+          <p>{props.position}</p>
+          <p>{props.description}</p>
         </div>
       </div>
     </>
