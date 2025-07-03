c957ae249ed27a0ad51cdbfe7e59c718d31b4f09
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-07-03 18:34:34 +0800

    refactor: replace card elements with ServiceCard components for better code reusability

--- components/ui/home/ServiceBenifits.tsx
+++ components/ui/home/ServiceBenifits.tsx
@@ -1,56 +1,29 @@
-import React from "react";
+import ServiceCard from "./ServiceCard";
 
 const ServiceBenifits = () => {
   return (
     <div className="flex flex-col md:flex-row justify-center items-center px-4 lg:px-12 text-center gap-4 lg:gap-8 py-12">
-      <div className="card bg-base-100 w-auto md:h-70 xl:h-90 lg:w-96 shadow-md shadow-red-500/50 hover:shadow-xl">
-        <figure className="px-10 pt-10">
-          <i className="bi bi-clock text-red-600 text-xl md:text-3xl xl:text-5xl"></i>
-        </figure>
-        <div className="card-body items-center text-center">
-          <h1 className="font-family-mono text-md md:text-lg xl:text-xl font-bold">
-            Availability
-          </h1>
-          <p className="font-family-sans text-xs md:text-md xl:text-lg text-justify ">
-            InstaDrive offers a wide selection of vehicles ready to rent
-            anytime, with real-time booking and flexible pick-up options.
-            Whether it’s a last-minute trip or a planned vacation, we’re here
-            24/7.
-          </p>
-        </div>
-      </div>
+      <ServiceCard
+        title="Availability"
+        description="InstaDrive offers a wide selection of vehicles ready to rent anytime, with real-time booking and flexible pick-up options. Whether it’s a last-minute trip or a planned vacation, we’re here 24/7."
+        icon="bi bi-clock"
+      />
 
-      <div className="card bg-base-100 w-auto md:h-70 xl:h-90 lg:w-96 shadow-md shadow-red-500/50 hover:shadow-xl">
-        <figure className="px-10 pt-10">
-          <i className="bi bi-car-front-fill text-xl text-red-600 md:text-3xl xl:text-5xl"></i>
-        </figure>
-        <div className="card-body items-center text-center">
-          <h1 className="font-family-mono text-md md:text-lg xl:text-xl font-bold">
-            Comfort
-          </h1>
-          <p className="font-family-sans text-xs md:text-md xl:text-lg text-justify ">
-            Our fleet is well-maintained, clean, and equipped with modern
+      <ServiceCard
+        title="Comfort"
+        description="Our fleet is well-maintained, clean, and equipped with modern
             features to ensure your driving experience is relaxing and
-            enjoyable—no matter the distance.
-          </p>
-        </div>
-      </div>
+            enjoyable—no matter the distance."
+        icon="bi-car-front-fill"
+      />
 
-      <div className="card bg-base-100 w-auto md:h-70 xl:h-90 lg:w-96 shadow-md shadow-red-500/50 hover:shadow-xl">
-        <figure className="px-10 pt-10">
-          <i className="bi bi-currency-dollar text-xl text-red-600 md:text-3xl  xl:text-5xl"></i>
-        </figure>
-        <div className="card-body items-center text-center">
-          <h1 className="font-family-mono text-md md:text-lg xl:text-xl font-bold">
-            Savings
-          </h1>
-          <p className="font-family-sans text-xs md:text-md xl:text-lg text-justify ">
-            Enjoy transparent pricing, zero hidden fees, and exclusive deals. We
+      <ServiceCard
+        title="Savings"
+        description="Enjoy transparent pricing, zero hidden fees, and exclusive deals. We
             make renting a car budget-friendly, so you get more mileage for your
-            money.
-          </p>
-        </div>
-      </div>
+            money."
+        icon="bi bi-currency-dollar"
+      />
     </div>
   );
 };
