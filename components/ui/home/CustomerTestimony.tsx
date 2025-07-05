ddf613412d47c6ed3dd25c065a3add6080ddd35d
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-07-05 14:12:02 +0800

    feat: implement Swiper for CustomerTestimony component to enhance user experience

--- components/ui/home/CustomerTestimony.tsx
+++ components/ui/home/CustomerTestimony.tsx
@@ -1,120 +1,97 @@
-import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 const CustomerTestimony = () => {
   return (
-    <div className="carousel w-full delay-100 autoplay">
-      <div
-        id="slide1"
-        className="carousel-item w-full flex justify-center items-center flex-col gap-4"
+    <div className="w-full max-w-6xl mx-auto">
+      <Swiper
+        modules={[Pagination, Autoplay, Navigation]}
+        spaceBetween={10}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          1536: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+          },
+        }}
       >
-        <div className="flex flex-col justify-center items-center pt-12">
-          <div className="card w-auto bg-base-100 card-xl shadow-sm">
+        <SwiperSlide className="carousel-item w-full flex justify-center items-center flex-col gap-4">
+          <div className="card w-96 bg-base-100 card-md shadow-sm mb-8">
             <div className="card-body">
-              <p className="text-center text-md sm:text-xl sm:max-w-xl max-w-xs md:text-2xl md:max-w-2xl lg:text-4xl lg:max-w-4xl mb-8">
-                "I had an amazing experience with InstaDrive! The booking
-                process was seamless, and the car was in excellent condition.
-                Highly recommend!"
+              <h2 className="card-title">Medium Card</h2>
+              <p>
+                A card component has a figure, a body part, and inside body
+                there are title and actions parts
               </p>
-
-              <p className="text-center text-md sm:text-xl sm:max-w-xl max-w-xs md:text-xl md:max-w-xl lg:text-3xl lg:max-w-3xl">
-                - John Cena
-              </p>
+              <div className="justify-end card-actions">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className="items-center gap-8 flex justify-center">
-          <div>
-            <a
-              href="#slide3"
-              className="btn btn-circle btn-xs sm:btn-sm md:btn-md lg:btn-lg"
-            >
-              ❮
-            </a>
-          </div>
-
-          <div>
-            <a
-              href="#slide2"
-              className="btn btn-circle btn-xs sm:btn-sm md:btn-md lg:btn-lg"
-            >
-              ❯
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div
-        id="slide2"
-        className="carousel-item w-full flex justify-center items-center flex-col gap-4"
-      >
-        <div className="flex flex-col justify-center items-center pt-12">
-          <div className="card w-auto bg-base-100 card-xl shadow-sm">
+        </SwiperSlide>
+        <SwiperSlide className="carousel-item w-full flex justify-center items-center flex-col gap-4">
+          <div className="card w-96 bg-base-100 card-md shadow-sm">
             <div className="card-body">
-              <p className="text-center text-md sm:text-xl sm:max-w-xl max-w-xs md:text-2xl md:max-w-2xl lg:text-4xl lg:max-w-4xl mb-8">
-                "I had an amazing experience with InstaDrive! The booking
-                process was seamless, and the car was in excellent condition.
-                Highly recommend!"
+              <h2 className="card-title">Medium Card</h2>
+              <p>
+                A card component has a figure, a body part, and inside body
+                there are title and actions parts
               </p>
-
-              <p className="text-center text-md sm:text-xl sm:max-w-xl max-w-xs md:text-xl md:max-w-xl lg:text-3xl lg:max-w-3xl">
-                - John Cena
-              </p>
+              <div className="justify-end card-actions">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className="items-center gap-8 flex justify-center">
-          <a
-            href="#slide1"
-            className="btn btn-circle btn-xs sm:btn-sm md:btn-md lg:btn-lg"
-          >
-            ❮
-          </a>
-          <a
-            href="#slide3"
-            className="btn btn-circle btn-xs sm:btn-sm md:btn-md lg:btn-lg"
-          >
-            ❯
-          </a>
-        </div>
-      </div>
-
-      <div
-        id="slide3"
-        className="carousel-item w-full flex justify-center items-center flex-col gap-4"
-      >
-        <div className="flex flex-col justify-center items-center pt-12">
-          <div className="card w-auto bg-base-100 card-xl shadow-sm">
+        </SwiperSlide>
+        <SwiperSlide className="carousel-item w-full flex justify-center items-center flex-col gap-4">
+          <div className="card w-96 bg-base-100 card-md shadow-sm">
             <div className="card-body">
-              <p className="text-center text-md sm:text-xl sm:max-w-xl max-w-xs md:text-2xl md:max-w-2xl lg:text-4xl lg:max-w-4xl mb-8">
-                "I had an amazing experience with InstaDrive! The booking
-                process was seamless, and the car was in excellent condition.
-                Highly recommend!"
+              <h2 className="card-title">Medium Card</h2>
+              <p>
+                A card component has a figure, a body part, and inside body
+                there are title and actions parts
               </p>
-
-              <p className="text-center text-md sm:text-xl sm:max-w-xl max-w-xs md:text-xl md:max-w-xl lg:text-3xl lg:max-w-3xl">
-                - John Cena
-              </p>
+              <div className="justify-end card-actions">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className="items-center gap-8 flex justify-center">
-          <a
-            href="#slide2"
-            className="btn btn-circle btn-xs sm:btn-sm md:btn-md lg:btn-lg"
-          >
-            ❮
-          </a>
-          <a
-            href="#slide1"
-            className="btn btn-circle btn-xs sm:btn-sm md:btn-md lg:btn-lg"
-          >
-            ❯
-          </a>
-        </div>
-      </div>
+        </SwiperSlide>
+        <SwiperSlide className="carousel-item w-full flex justify-center items-center flex-col gap-4">
+          <div className="card w-96 bg-base-100 card-md shadow-sm">
+            <div className="card-body">
+              <h2 className="card-title">Medium Card</h2>
+              <p>
+                A card component has a figure, a body part, and inside body
+                there are title and actions parts
+              </p>
+              <div className="justify-end card-actions">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
