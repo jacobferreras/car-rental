6ae65838f0f1b55ebf897dddb08d9b2525fd399c
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-07-07 18:12:26 +0800

    refactor: update CustomerTestimony component layout and styling with improved text formatting and image inclusion

--- components/ui/home/CustomerTestimony.tsx
+++ components/ui/home/CustomerTestimony.tsx
@@ -32,10 +32,10 @@ const CustomerTestimony = () => {
           <div className="card w-auto bg-base-100 card-md shadow-sm mb-12">
             <div className="card-body">
               <h2 className="card-title justify-center items-center text-center">
-                A card component has a figure, a body part, and inside body
-                there are title and actions parts
+                "A card component has a figure, a body part, and inside body
+                there are title and actions parts"
               </h2>
-              <div className="flex justify-center items-center mb-4">
+              <div className="flex justify-center items-center py-4">
                 <svg
                   className="w-6 h-6 star-filled text-yellow-300"
                   fill="currentColor"
@@ -65,8 +65,16 @@ const CustomerTestimony = () => {
                   <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.785.57-1.84-.197-1.54-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.462a1 1 0 00.95-.69l1.07-3.292z"></path>
                 </svg>
               </div>
-              <p className="justify-center items-center text-center">
-                - John Doe
+
+              <figure className="px-10">
+                <img
+                  src="/Fleet.jpg"
+                  alt="Customer"
+                  className="rounded-full w-20 h-20 object-cover"
+                />
+              </figure>
+              <p className="justify-center items-center text-center text-3xl font-family-mono font-bold">
+                John Doe
               </p>
             </div>
           </div>
@@ -75,17 +83,10 @@ const CustomerTestimony = () => {
           <div className="card w-auto bg-base-100 card-md shadow-sm mb-12">
             <div className="card-body">
               <h2 className="card-title justify-center items-center text-center">
-                A card component has a figure, a body part, and inside body
-                there are title and actions parts
+                "A card component has a figure, a body part, and inside body
+                there are title and actions parts"
               </h2>
-              <div className="flex justify-center items-center mb-4">
-                <svg
-                  className="w-6 h-6 star-filled text-yellow-300"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.785.57-1.84-.197-1.54-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.462a1 1 0 00.95-.69l1.07-3.292z"></path>
-                </svg>
+              <div className="flex justify-center items-center py-4">
                 <svg
                   className="w-6 h-6 star-filled text-yellow-300"
                   fill="currentColor"
@@ -115,8 +116,15 @@ const CustomerTestimony = () => {
                   <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.785.57-1.84-.197-1.54-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.462a1 1 0 00.95-.69l1.07-3.292z"></path>
                 </svg>
               </div>
-              <p className="justify-center items-center text-center">
-                - John Doe
+              <figure className="px-10">
+                <img
+                  src="/Fleet.jpg"
+                  alt="Customer"
+                  className="rounded-full w-20 h-20 object-cover"
+                />
+              </figure>
+              <p className="justify-center items-center text-center text-3xl font-family-mono font-bold">
+                John Doe
               </p>
             </div>
           </div>
@@ -125,10 +133,10 @@ const CustomerTestimony = () => {
           <div className="card w-auto bg-base-100 card-md shadow-sm mb-12">
             <div className="card-body">
               <h2 className="card-title justify-center items-center text-center">
-                A card component has a figure, a body part, and inside body
-                there are title and actions parts
+                "A card component has a figure, a body part, and inside body
+                there are title and actions parts"
               </h2>
-              <div className="flex justify-center items-center mb-4">
+              <div className="flex justify-center items-center py-4">
                 <svg
                   className="w-6 h-6 star-filled text-yellow-300"
                   fill="currentColor"
@@ -165,8 +173,15 @@ const CustomerTestimony = () => {
                   <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.785.57-1.84-.197-1.54-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.462a1 1 0 00.95-.69l1.07-3.292z"></path>
                 </svg>
               </div>
-              <p className="justify-center items-center text-center">
-                - John Doe
+              <figure className="px-10">
+                <img
+                  src="/Fleet.jpg"
+                  alt="Customer"
+                  className="rounded-full w-20 h-20 object-cover"
+                />
+              </figure>
+              <p className="justify-center items-center text-center text-3xl font-family-mono font-bold">
+                John Doe
               </p>
             </div>
           </div>
@@ -175,10 +190,10 @@ const CustomerTestimony = () => {
           <div className="card w-auto bg-base-100 card-md shadow-sm mb-12">
             <div className="card-body">
               <h2 className="card-title justify-center items-center text-center">
-                A card component has a figure, a body part, and inside body
-                there are title and actions parts
+                "A card component has a figure, a body part, and inside body
+                there are title and actions parts"
               </h2>
-              <div className="flex justify-center items-center mb-4">
+              <div className="flex justify-center items-center py-4">
                 <svg
                   className="w-6 h-6 star-filled text-yellow-300"
                   fill="currentColor"
@@ -215,8 +230,15 @@ const CustomerTestimony = () => {
                   <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.785.57-1.84-.197-1.54-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.462a1 1 0 00.95-.69l1.07-3.292z"></path>
                 </svg>
               </div>
-              <p className="justify-center items-center text-center">
-                - John Doe
+              <figure className="px-10">
+                <img
+                  src="/Fleet.jpg"
+                  alt="Customer"
+                  className="rounded-full w-16 h-16 object-cover"
+                />
+              </figure>
+              <p className="justify-center items-center text-center text-xl font-family-mono font-bold">
+                John Doe
               </p>
             </div>
           </div>
