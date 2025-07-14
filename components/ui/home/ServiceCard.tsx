891393090774d7d399b88af7fcc977b92edb6324
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-07-14 15:49:43 +0800

    refactor: adjust responsive styles in ServiceCard for improved layout consistency

--- components/ui/home/ServiceCard.tsx
+++ components/ui/home/ServiceCard.tsx
@@ -6,17 +6,20 @@ interface ServiceCardProps {
 
 const ServiceCard = (props: ServiceCardProps) => {
   return (
-    <div className="card bg-base-100 w-auto md:h-70 xl:h-90 md:w-106 shadow-md shadow-red-500/50 hover:shadow-xl">
+    <div className="card bg-base-100 w-auto md:h-70  lg:h-90 md:w-106 shadow-md shadow-red-500/50 hover:shadow-xl">
       <figure className="px-10 pt-10">
         <i
           className={`${props.icon} text-red-600 text-xl md:text-3xl xl:text-5xl`}
         ></i>
       </figure>
       <div className="card-body items-center text-center">
-        <h1 className="font-family-mono text-md md:text-lg xl:text-xl font-bold">
+        <h1
+          className="font-family-mono
+         sm:text-sm md:text-md xl:text-xl font-bold"
+        >
           {props.title}
         </h1>
-        <p className="font-family-sans text-xs md:text-md xl:text-lg text-justify ">
+        <p className="font-family-sans sm:text-sm md:text-md lg:text-lg xl:text-xl text-justify ">
           {props.description}
         </p>
       </div>
