b0d336cf3811434ac1fa85abb4e8925b2a1209e8
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-07-15 15:25:41 +0800

    refactor: adjust width and text size in MissionAndVisionCard for improved layout consistency

--- components/ui/about/MissionAndVisionCard.tsx
+++ components/ui/about/MissionAndVisionCard.tsx
@@ -7,7 +7,7 @@ interface MissionAndVisionCardProps {
 const MissionAndVisionCard = (props: MissionAndVisionCardProps) => {
   return (
     <>
-      <div className="card bg-base-100 w-auto md:h-100 xl:h-90 lg:w-110 xl:w-130 shadow-sm">
+      <div className="card bg-base-100 w-auto md:h-100 xl:h-90 lg:w-110 xl:w-160 shadow-sm">
         <div className="card-header flex justify-center items-center pt-8">
           <i
             className={`${props.iconClass} text-2xl text-red-600 md:text-3xl xl:text-5xl`}
@@ -18,7 +18,7 @@ const MissionAndVisionCard = (props: MissionAndVisionCardProps) => {
           <h1 className="font-family-mono text-md md:text-lg xl:text-3xl font-bold">
             {props.title}
           </h1>
-          <p className="font-family-sans text-xs md:text-lg xl:text-xl text-justify ">
+          <p className="font-family-sans text-xs md:text-md lg:text-lg xl:text-xl text-justify ">
             {props.description}
           </p>
         </div>
