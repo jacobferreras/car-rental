8f4762a866cb82044e8cb2ca699a2c0ba56c758d
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-08-09 14:05:01 +0800

    refactor: update header text in VehicleSection for improved clarity and appeal

--- components/ui/vehicles/VehicleSection.tsx
+++ components/ui/vehicles/VehicleSection.tsx
@@ -44,10 +44,11 @@ const VehicleSection = () => {
         <div className="absolute inset-0 bg-[#0b0e13] opacity-70 z-0"></div>
         <div className="flex flex-col justify-center items-center absolute z-20">
           <h1 className="text-3xl md:text-5xl lg:text-7xl text-center text-white font-bold font-family-mono">
-            ABOUT INSTADRIVE
+            OUR EXCEPTIONAL FLEET
           </h1>
-          <h2 className="text-3xl md:text-5xl lg:text-6xl text-center text-white font-bold font-family-mono">
-            WITH INSTADRIVE
+          <h2 className="text-3xl md:text-5xl lg:text-3xl text-center text-white font-bold font-family-mono">
+            Explore our curated collection of the world's most prestigious and
+            exhilarating luxury vehicles.
           </h2>
         </div>
       </div>
