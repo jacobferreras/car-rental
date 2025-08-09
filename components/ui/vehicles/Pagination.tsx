eaf92d702f93024bc546806696838b076a74249f
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-08-09 15:33:44 +0800

    style: add hover effect to radio buttons in Pagination component for improved user interaction

--- components/ui/vehicles/Pagination.tsx
+++ components/ui/vehicles/Pagination.tsx
@@ -18,7 +18,7 @@ const Pagination = ({
       {Array.from({ length: totalPages }, (_, idx) => (
         <input
           key={idx + 1}
-          className="join-item btn btn-square bg-[#1c2634] text-white "
+          className="join-item btn btn-square bg-[#1c2634] text-white hover:bg-[#1d4ed8] border-0"
           type="radio"
           name="options"
           aria-label={(idx + 1).toString()}
