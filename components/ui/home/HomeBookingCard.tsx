d28a912828d8216998543fb41eb2bfab7e5bac47
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-08-12 20:20:13 +0800

    fix: improve styling of HomeBookingCard component for better layout and visibility

--- components/ui/home/HomeBookingCard.tsx
+++ components/ui/home/HomeBookingCard.tsx
@@ -5,20 +5,20 @@ const HomeBookingCard = () => {
   return (
     <div className="card card-border bg-[#090b0f] bg-opacity-50 w-70 sm:w-150 lg:w-200 ">
       <div className="card-body">
-        <h1 className="card-title justify-center text-[#5897e4] sm:text-2xl md:text-3xl">
+        <h1 className="card-title justify-center text-[#5897e4] sm:text-2xl md:text-3xl mb-2">
           Find Your Perfect Car
         </h1>
         <div className="flex flex-col sm:flex-row gap-4">
           <div>
-            <h2 className="text-[#5897e4] lg:text-xl">Pick-up Date</h2>
+            <h2 className="text-[#acafb5] lg:text-xl mb-2">Pick-up Date</h2>
             <DateInput />
           </div>
           <div>
-            <h2 className="text-[#5897e4] lg:text-xl">Drop-off Date</h2>
+            <h2 className="text-[#acafb5] lg:text-xl mb-2">Drop-off Date</h2>
             <DateInput />
           </div>
 
-          <div className="sm:pt-7">
+          <div className="sm:pt-9">
             <button className="btn bg-[#1d4ed8]">Medium</button>
           </div>
         </div>
