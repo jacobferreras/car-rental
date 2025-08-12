7fa5bd5e403cde70ee933fcb41e1ebf2c53cf419
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-08-12 20:14:12 +0800

    fix: enhance DateInput component styling with responsive width

--- components/common/DateInput.tsx
+++ components/common/DateInput.tsx
@@ -3,7 +3,10 @@ import React from "react";
 const DateInput = () => {
   return (
     <div>
-      <input type="date" className="input input-bordered w-full sm:w-54" />
+      <input
+        type="date"
+        className="input input-bordered w-full sm:w-54 lg:w-78"
+      />
     </div>
   );
 };
