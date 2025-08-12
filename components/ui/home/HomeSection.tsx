31dea4e3988d27f3b6c5c71ced28dc66a6410c4e
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-08-12 12:33:56 +0800

    fix: update HomeSection component with enhanced header and descriptive text

--- components/ui/home/HomeSection.tsx
+++ components/ui/home/HomeSection.tsx
@@ -29,11 +29,12 @@ const HomeSection = () => {
         <div className="absolute inset-0 bg-[#0b0e13] opacity-70 z-0"></div>
         <div className="flex flex-col justify-center items-center absolute z-20">
           <h1 className="text-3xl sm:text-5xl lg:text-7xl text-center text-white font-family-mono font-bold z-20">
-            DRIVE YOUR JOURNEY
+            DRIVE YOUR JOURNEY WITH INSTADRIVE
           </h1>
-          <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center text-white font-family-mono font-bold z-20">
-            WITH INSTADRIVE
-          </h2>
+          <p className="text-md sm:text-lg lg:text-xl text-center text-white font-family-sans mt-4 px-4 z-20">
+            Experience unparalleled luxury and performance with our exclusive
+            fleet of premium vehicles, curated for the discerning driver.
+          </p>
           <div className="flex justify-center items-center mt-4 px-4">
             <HomeBookingCard />
           </div>
