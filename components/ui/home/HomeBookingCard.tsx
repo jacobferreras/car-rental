a9cdce6eb5f86c64b1d0b2ab26d7cb5bee5768ff
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-08-12 20:14:16 +0800

    fix: update HomeBookingCard component for improved layout and text styling

--- components/ui/home/HomeBookingCard.tsx
+++ components/ui/home/HomeBookingCard.tsx
@@ -3,22 +3,22 @@ import React from "react";
 
 const HomeBookingCard = () => {
   return (
-    <div className="card card-border bg-[#090b0f] bg-opacity-50 w-70 sm:w-150 ">
+    <div className="card card-border bg-[#090b0f] bg-opacity-50 w-70 sm:w-150 lg:w-200 ">
       <div className="card-body">
         <h1 className="card-title justify-center text-[#5897e4] sm:text-2xl md:text-3xl">
           Find Your Perfect Car
         </h1>
         <div className="flex flex-col sm:flex-row gap-4">
           <div>
-            <h2 className="text-[#5897e4]">Pick-up Date</h2>
+            <h2 className="text-[#5897e4] lg:text-xl">Pick-up Date</h2>
             <DateInput />
           </div>
           <div>
-            <h2 className="text-[#5897e4]">Drop-off Date</h2>
+            <h2 className="text-[#5897e4] lg:text-xl">Drop-off Date</h2>
             <DateInput />
           </div>
 
-          <div className="sm:pt-5">
+          <div className="sm:pt-7">
             <button className="btn bg-[#1d4ed8]">Medium</button>
           </div>
         </div>
