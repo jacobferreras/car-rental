ca10961e57dd1bc03c3a6fc051041509d30a8b71
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-08-12 11:45:11 +0800

    fix: update HomeBookingCard layout for responsive design adjustments

--- components/ui/home/HomeBookingCard.tsx
+++ components/ui/home/HomeBookingCard.tsx
@@ -3,12 +3,12 @@ import React from "react";
 
 const HomeBookingCard = () => {
   return (
-    <div className="card card-border bg-[#090b0f] bg-opacity-50 w-auto lg:w-106">
+    <div className="card card-border bg-[#090b0f] bg-opacity-50 w-auto sm:w-106 ">
       <div className="card-body">
         <h1 className="card-title justify-center text-[#5897e4]">
           Find Your Perfect Car
         </h1>
-        <div className="flex flex-row gap-4">
+        <div className="flex flex-col sm:flex-row gap-4">
           <div>
             <h2 className="text-[#5897e4]">Pick-up Date</h2>
             <DateInput />
@@ -18,7 +18,7 @@ const HomeBookingCard = () => {
             <DateInput />
           </div>
 
-          <div className="pt-5">
+          <div className="sm:pt-5">
             <button className="btn bg-[#1d4ed8]">Medium</button>
           </div>
         </div>
