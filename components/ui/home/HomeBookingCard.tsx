e6c574bd4acb1099b2dbb41a0875f88c4734f949
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-08-14 10:40:53 +0800

    fix: wrap Search Car button with Link component for navigation

--- components/ui/home/HomeBookingCard.tsx
+++ components/ui/home/HomeBookingCard.tsx
@@ -1,5 +1,6 @@
 import DateInput from "@/components/common/DateInput";
 import React from "react";
+import Link from "next/link";
 
 const HomeBookingCard = () => {
   return (
@@ -19,7 +20,9 @@ const HomeBookingCard = () => {
           </div>
 
           <div className="sm:pt-9">
-            <button className="btn bg-[#1d4ed8] w-26">Search Car</button>
+            <Link href="/vehicles">
+              <button className="btn bg-[#1d4ed8] w-26">Search Car</button>
+            </Link>
           </div>
         </div>
       </div>
