3702413e2995ee782240998971ccfdae7be91b8f
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-08-14 20:04:29 +0800

    fix: update layout structure in VehicleDetails component for improved styling

--- components/ui/vehicles/VehicleDetails.tsx
+++ components/ui/vehicles/VehicleDetails.tsx
@@ -11,36 +11,39 @@ const VehicleDetails = () => {
 
   return (
     <>
-      <div className="mt-24 px-4">
-        <div className="card lg:card-side bg-base-100 shadow-sm">
-          <figure>
-            <Image
-              src={car.imageUrl.trim()}
-              alt={car.make}
-              className="h-60 w-full"
-              width="1000"
-              height="1000"
-            />
-          </figure>
-          <div className="card-body w-full">
-            <h2 className="card-title text-6xl">
-              <span>{car.make}</span>
-              {car.model}
-            </h2>
-            <div>
-              <p>STARTING AT</p>
-              <p className="text-2xl">₱{car.pricePerDay}/Day</p>
-            </div>
-            <div>
-              <p className="text-xl">{car.description}</p>
-            </div>
-            <div>
-              <button className="btn btn-primary">Rent Now</button>
+      <div className="min-h-screen bg-[#030712]">
+        <div className="rounded-xl card justify-center items-center mx-4 bg-[#1f2937] mt-20">
+          <div className="card lg:card-side bg-base-100 shadow-sm">
+            <figure>
+              <Image
+                src={car.imageUrl.trim()}
+                alt={car.make}
+                className="h-60 w-full"
+                width="1000"
+                height="1000"
+              />
+            </figure>
+            <div className="card-body w-full">
+              <h2 className="card-title text-6xl">
+                <span>{car.make}</span>
+                {car.model}
+              </h2>
+              <div>
+                <p>STARTING AT</p>
+                <p className="text-2xl">₱{car.pricePerDay}/Day</p>
+              </div>
+              <div>
+                <p className="text-xl">{car.description}</p>
+              </div>
+              <div>
+                <button className="btn btn-primary">Rent Now</button>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="text-6xl py-6">Car Details</div>
+          <div className="text-6xl py-6">Car Details</div>
+          <p>{car.color}</p>
+        </div>
       </div>
     </>
   );
