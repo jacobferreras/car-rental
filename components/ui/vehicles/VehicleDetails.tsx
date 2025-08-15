1db71c8692b62c2241fdd5dd1e79b0edc22a2719
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-08-16 00:06:24 +0800

    fix: improve layout and styling in VehicleDetails component for better readability and structure

--- components/ui/vehicles/VehicleDetails.tsx
+++ components/ui/vehicles/VehicleDetails.tsx
@@ -12,10 +12,10 @@ const VehicleDetails = () => {
   return (
     <div className="min-h-screen bg-[#030712]">
       <div className="pt-24 pb-12">
-        <div className="rounded-xl mx-4 bg-[#1f2937] ">
+        <div className="rounded-xl mx-12 bg-[#1f2937] ">
           <h1 className="pl-6 pt-4 text-2xl font-bold">Vehicle Details</h1>
           <div className="h-px w-full bg-gray-600 my-4" />
-          <div className="flex flex-col md:flex-row pb-8">
+          <div className="flex flex-col md:flex-row pb-8 gap-8">
             <figure className="pl-4 pt-10 w-auto  md:w-200">
               <img
                 src={car.imageUrl}
@@ -24,29 +24,38 @@ const VehicleDetails = () => {
               />
             </figure>
             <div className="card-body items-start">
-              <h1 className="card-title text-4xl">
-                {car.make} {car.model}
-              </h1>
-              <h2 className="text-3xl text-[#60a5fa]">
-                ₱{car.pricePerDay}/day
-              </h2>
-              <h2 className="text-3xl">Description</h2>
-              <p className="text-start w-200 text-2xl text-[#d1d5db]">
-                {car.description}
-              </p>
-              <h2 className="text-3xl">Key Specification</h2>
-              <p className="text-start w-200 text-2xl text-[#d1d5db]">
-                Engine: {car.engine}
-              </p>
-              <p className="text-start w-200 text-2xl text-[#d1d5db]">
-                Horse Power: {car.horsepower}hp
-              </p>
-              <p className="text-start w-200 text-2xl text-[#d1d5db]">
-                Mileage: {car.mileage}miles
-              </p>
-              <p className="text-start w-200 text-2xl text-[#d1d5db]">
-                Color: {car.color}
-              </p>
+              <div className="pt-2 mb-2">
+                <h1 className="card-title text-4xl mb-2">
+                  {car.make} {car.model}
+                </h1>
+                <h2 className="text-2xl text-[#60a5fa] mb-2">
+                  ₱{car.pricePerDay}/day
+                </h2>
+                <h2 className="text-2xl mb-2">Description</h2>
+                <p className="text-start w-200 text-lg text-[#d1d5db]">
+                  {car.description}
+                </p>
+              </div>
+
+              <div className="mb-4">
+                <h2 className="text-2xl mb-2">Key Specification</h2>
+                <p className="text-start w-200 text-lg text-[#d1d5db]">
+                  Engine: {car.engine}
+                </p>
+                <p className="text-start w-200 text-lg text-[#d1d5db]">
+                  Horse Power: {car.horsepower}hp
+                </p>
+                <p className="text-start w-200 text-lg text-[#d1d5db]">
+                  Mileage: {car.mileage}miles
+                </p>
+                <p className="text-start w-200 text-lg text-[#d1d5db]">
+                  Color: {car.color}
+                </p>
+              </div>
+
+              <button className="btn bg-[#1d4ed8] rounded-xl">
+                Book Your Ride Now <i className="bi bi-arrow-right"></i>
+              </button>
             </div>
           </div>
         </div>
