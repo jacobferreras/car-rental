0711cfbc5daed7bf41232dab7ab76b12c6a89a4c
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-08-19 19:35:47 +0800

    feat: integrate CallToActionSection into HomeSection and remove redundant markup

--- components/ui/home/HomeSection.tsx
+++ components/ui/home/HomeSection.tsx
@@ -1,8 +1,8 @@
-import Link from "next/link";
 import Hero from "./HeroSection";
 import VehicleSection from "./VehicleSection";
 import WhyChooseUsSection from "./WhyChooseUsSection";
 import TestimonialSection from "./TestimonialSection";
+import CallToActionSection from "../../common/CallToActionSection";
 
 const HomeSection = async () => {
   return (
@@ -11,25 +11,7 @@ const HomeSection = async () => {
       <VehicleSection />
       <WhyChooseUsSection />
       <TestimonialSection />
-
-      <div>
-        <div className="flex flex-col justify-center items-center px-2 py-24 bg-[#1d2735]">
-          <div className="text-2xl lg:text-5xl font-family-mono font-bold mb-4 text-white pl-4">
-            Ready to Experience Seamless Travel?
-          </div>
-          <div className="text-md lg:text-xl font-family-sans text-white text-center mb-8 px-4">
-            Browse our extensive fleet and find the perfect car for your next
-            adventure.
-          </div>
-          <div className="flex justify-center items-center">
-            <Link href="/vehicles">
-              <button className="btn bg-[#1d4ed8] rounded-xl">
-                Book Your Ride Now <i className="bi bi-arrow-right"></i>
-              </button>
-            </Link>
-          </div>
-        </div>
-      </div>
+      <CallToActionSection />
     </div>
   );
 };
