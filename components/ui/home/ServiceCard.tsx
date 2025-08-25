679cf83d94ec2cc348e1509660f479b6a3ed8d9a
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-08-25 14:00:07 +0800

    feat: adjust spacing and text styles in ServiceBenifits and WhyChooseUsSection components

--- components/ui/home/ServiceCard.tsx
+++ components/ui/home/ServiceCard.tsx
@@ -8,18 +8,16 @@ const ServiceCard = (props: ServiceCardProps) => {
   return (
     <div className="card bg-[#303a49] w-auto md:h-70  lg:h-90 md:w-106 shadow-md shadow-blue-600/50 hover:shadow-xl">
       <figure className="px-10 pt-10">
-        <i
-          className={`${props.icon} text-[#93c5fd] text-xl md:text-3xl xl:text-5xl`}
-        ></i>
+        <i className={`${props.icon} text-[#93c5fd] text-5xl`}></i>
       </figure>
       <div className="card-body items-center text-center">
         <h1
           className="font-family-mono
-         sm:text-sm md:text-md xl:text-xl font-bold"
+         text-lg xl:text-xl font-bold"
         >
           {props.title}
         </h1>
-        <p className="font-family-sans sm:text-sm md:text-md lg:text-lg xl:text-xl">
+        <p className="font-family-sans text-md lg:text-lg xl:text-xl">
           {props.description}
         </p>
       </div>
--- components/ui/home/WhyChooseUsSection.tsx
+++ components/ui/home/WhyChooseUsSection.tsx
@@ -4,9 +4,9 @@ import ServiceBenifits from "./ServiceBenifits";
 const WhyChooseUsSection = () => {
   return (
     <div>
-      <div className="bg-[#1f2937] py-24">
-        <div className="flex justify-center items-center text-2xl lg:text-5xl font-family-mono font-bold  text-white pl-4 pt-12">
-          Why Choose InstaDrive?
+      <div className="bg-[#1f2937] py-12">
+        <div className="flex justify-center items-center text-2xl lg:text-5xl font-family-mono font-bold  text-white pl-4 ">
+          WHY CHOOSE INSTADRIVE?
         </div>
         <ServiceBenifits />
       </div>
