3702eb904e6ec00b5badff73545a6ddfd3174ae8
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-08-26 01:01:18 +0800

    feat: center align text in CallToActionSection component

--- components/common/CallToActionSection.tsx
+++ components/common/CallToActionSection.tsx
@@ -6,7 +6,7 @@ const CallToActionSection = () => {
     <div>
       <div>
         <div className="flex flex-col justify-center items-center px-2 py-24 bg-[#1d2735]">
-          <div className="text-2xl lg:text-5xl font-family-mono font-bold mb-4 text-white pl-4">
+          <div className="text-2xl lg:text-5xl font-family-mono font-bold mb-4 text-white pl-4 text-center">
             Ready to Experience Seamless Travel?
           </div>
           <div className="text-md lg:text-xl font-family-sans text-white text-center mb-8 px-4">
