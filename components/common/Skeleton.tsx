b6afd03d5f3dd831a1f4dd44fffd49733f69be89
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-08-26 19:58:22 +0800

    feat: update Skeleton component width and add animation effect

--- components/common/Skeleton.tsx
+++ components/common/Skeleton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Skeleton = () => {
   return (
-    <div className="flex w-52 flex-col gap-4">
+    <div className="flex w-106 flex-col gap-4 animate-pulse">
       <div className="skeleton h-32 w-full"></div>
       <div className="skeleton h-4 w-28"></div>
       <div className="skeleton h-4 w-full"></div>
