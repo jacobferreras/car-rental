f3e804091437e1e6c454a1f9266ba3ca72fe1f1a
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-08-30 23:58:36 +0800

    feat: enhance BookingModal with form fields for car booking details

--- components/ui/vehicles/BookingModal.tsx
+++ components/ui/vehicles/BookingModal.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const BookingModal = () => {
-  return <div>BookingModal</div>;
+  return (
+    <fieldset className="fieldset bg-[#1a202c] border-base-300 rounded-box w-xs border p-4">
+      <legend className="fieldset-legend">Page details</legend>
+
+      <label className="label">Car Model</label>
+      <input type="text" className="input" placeholder="My awesome page" />
+
+      <label className="label">First Name</label>
+      <input type="text" className="input" placeholder="my-awesome-page" />
+
+      <label className="label">Last Name</label>
+      <input type="text" className="input" placeholder="Doe" />
+
+      <label className="label">Author</label>
+      <input type="text" className="input" placeholder="Name" />
+    </fieldset>
+  );
 };
 
 export default BookingModal;
