4831a4022e418c19536e0686945dba07e4ab586b
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-03 00:14:13 +0800

    refactor: update OurteamCard component to use destructured props for improved readability

--- components/ui/about/OurteamCard.tsx
+++ components/ui/about/OurteamCard.tsx
@@ -1,37 +1,45 @@
+import Image from "next/image";
+
 interface OurteamCardProps {
-  imageUrl?: string;
-  name?: string;
+  imageUrl: string;
+  name: string;
   position?: string;
   description?: string;
 }
 
-const OurteamCard = (props: OurteamCardProps) => {
+const OurteamCard = ({
+  imageUrl,
+  name,
+  position,
+  description,
+}: OurteamCardProps) => {
   return (
     <>
       <div className="card bg-[#303a49] w-auto md:h-150 lg:w-106  lg:h-120 xl:w-70 xl:h-144 shadow-sm">
         <figure className="px-10 pt-10">
-          <img
-            src={props.imageUrl}
-            alt={props.name}
+          <Image
+            src={imageUrl}
+            alt={name}
             className="rounded-full w-52 h-52 object-cover lg:w-35 lg:h-35 border-4 border-[#558cd2]"
+            width={150}
+            height={150}
+            priority
           />
         </figure>
         <div className="card-body items-center text-center">
           <div>
             {" "}
-            <h2 className="card-title font-bold font-family-mono">
-              {props.name}
-            </h2>
+            <h2 className="card-title font-bold font-family-mono">{name}</h2>
           </div>
 
           <div className="mb-4">
             <p className="text-[#558cd2] font-bold font-family-mono text-lg">
-              {props.position}
+              {position}
             </p>
           </div>
 
           <div>
-            <p className="font-family-sans text-lg">{props.description}</p>
+            <p className="font-family-sans text-lg">{description}</p>
           </div>
         </div>
       </div>
