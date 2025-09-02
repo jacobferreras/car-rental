6248363c177fb8dfa605c5c4fcb10b8180479205
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-02 23:57:45 +0800

    refactor: update Skeleton component to use flexible width for better responsiveness

--- components/common/Skeleton.tsx
+++ components/common/Skeleton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Skeleton = () => {
   return (
-    <div className="flex w-80 lg:w-106 flex-col gap-8 animate-pulse">
+    <div className="flex w-auto lg:w-106 flex-col gap-8 animate-pulse">
       <div className="skeleton h-56 w-full"></div>
       <div className="skeleton h-4 w-28"></div>
       <div className="skeleton h-4 w-28"></div>
