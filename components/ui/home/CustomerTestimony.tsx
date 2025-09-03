604151e554a0d8f937f319ae21e06687aeed96e0
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-03 11:35:00 +0800

    refactor: extract StarRating component for better reusability and separation of concerns

--- components/ui/home/CustomerTestimony.tsx
+++ components/ui/home/CustomerTestimony.tsx
@@ -6,23 +6,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import Image from "next/image";
-
-const StarRating = ({ rating }: { rating: number }) => {
-  return (
-    <div className="flex justify-center items-center py-4">
-      {[...Array(rating)].map((_, index) => (
-        <svg
-          key={index}
-          className="w-6 h-6 text-yellow-300"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-        >
-          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.785.57-1.84-.197-1.54-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.462a1 1 0 00.95-.69l1.07-3.292z" />
-        </svg>
-      ))}
-    </div>
-  );
-};
+import StarRating from "./StarRating";
 
 const CustomerTestimony = () => {
   return (
