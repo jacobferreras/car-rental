808b6235fa2b37b5d4f6979fccf2775cf340294f
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-03 23:32:31 +0800

    fix: correct font-family class name in HeroSection component

--- components/ui/home/HeroSection.tsx
+++ components/ui/home/HeroSection.tsx
@@ -15,7 +15,7 @@ const Hero = () => {
         />
         <div className="absolute inset-0 bg-[#0b0e13] opacity-70 z-0"></div>
         <div className="flex flex-col justify-center items-center absolute z-20">
-          <h1 className="text-3xl sm:text-5xl lg:text-7xl text-center text-white font-family-mono font-bold z-20">
+          <h1 className="text-3xl sm:text-5xl lg:text-7xl text-center text-white font-mono font-bold z-20">
             DRIVE YOUR JOURNEY WITH INSTADRIVE
           </h1>
           <p className="text-sm sm:text-xl lg:text-2xl text-center text-white font-family-sans mt-4 px-8 z-20">
