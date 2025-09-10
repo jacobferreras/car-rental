ccb6e2082ad8178fcd1d859ecf3618dac10f9a14
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-10 19:20:05 +0800

    fix: update Accordion component to use bold font for question title

--- components/ui/fqa/Accordion.tsx
+++ components/ui/fqa/Accordion.tsx
@@ -10,7 +10,9 @@ const Accordion = ({ question, answer }: AccordionProps) => {
     <>
       <div className="collapse collapse-arrow bg-[#111827] border border-base-300">
         <input type="radio" name="my-accordion-2" defaultChecked />
-        <div className="collapse-title font-semibold">{question}</div>
+        <div className="collapse-title font-bold text-[#3a97fa]">
+          {question}
+        </div>
         <div className="collapse-content text-sm">{answer}</div>
       </div>
     </>
