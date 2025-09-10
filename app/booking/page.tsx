882a89f77e36410ba471bc71f6a594c2445b9c33
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-11 06:10:02 +0800

    feat: add BookingContainer component for user booking display

---
+++ app/booking/page.tsx
@@ -0,0 +1,12 @@
+import BookingContainer from "@/components/ui/booking/BookingContainer";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <BookingContainer />
+    </>
+  );
+};
+
+export default page;
---
+++ components/ui/booking/BookingContainer.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const BookingContainer = async () => {
+  const bookings = await fetch(`${BASE_URL}/api/booking/user`).then((res) =>
+    res.json()
+  );
+
+  return (
+    <>
+      <div className="min-h-screen bg-[#111827] px-12 flex justify-center items-center">
+        <div className="px-2 py-24">
+          <div className="card w-auto bg-[#1f2937] card-xl shadow-sm">
+            <div className="card-body gap-4">
+              <h1 className="text-center text-4xl font-bold text-white mb-8">
+                My Bookings
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BookingContainer;
