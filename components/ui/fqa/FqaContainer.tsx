5f21bca73a6a21dd7caff59cd080adbd6ad761dd
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-10 19:16:25 +0800

    feat: add navigation link to contact page in FAQ section

--- components/ui/fqa/FqaContainer.tsx
+++ components/ui/fqa/FqaContainer.tsx
@@ -1,5 +1,7 @@
 import { faqs } from "@/lib/constant";
 import Accordion from "./Accordion";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+import Link from "next/link";
 
 const FqaContainer = () => {
   return (
@@ -8,9 +10,18 @@ const FqaContainer = () => {
         <div className="px-2 py-24">
           <div className="card w-auto bg-[#1f2937] card-xl shadow-sm">
             <div className="card-body gap-4">
+              <Link
+                href="/contact"
+                className="flex flex-row gap-2 cursor-pointer"
+              >
+                <FaArrowAltCircleLeft className="text-3xl text-white mb-4 hover:text-blue-500 cursor-pointer text-start" />
+                <h2>Go back</h2>
+              </Link>
+
               <h2 className="text-center text-5xl font-bold text-white mb-8">
                 Frequently Asked Questions
               </h2>
+
               {faqs.map((faq) => (
                 <Accordion
                   key={faq.id}
