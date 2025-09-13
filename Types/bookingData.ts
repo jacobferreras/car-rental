42b89012ca0c3aa1f61e4f6432a34a00bbd7632f
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-14 06:26:09 +0800

    fix: remove optional fields and simplify BookingData interface for consistency

--- Types/bookingData.ts
+++ Types/bookingData.ts
@@ -1,11 +1,9 @@
 export interface BookingData {
-  id?: number;
   email: string;
   firstName: string;
   lastName: string;
   carModel: string;
   carMake: string;
-  startDate: string | Date;
-  endDate: string | Date;
-  status?: "pending" | "confirmed" | "cancelled";
+  startDate: string;
+  endDate: string;
 }
